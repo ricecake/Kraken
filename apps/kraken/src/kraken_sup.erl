49e4e2a6c80e2cd8efb0eabad8a350da7229b6b7
Sebastian Green-Husted <ricecake@tfm.nu>
2013-05-19 17:54:05 -0400

    Initial checkin

---
+++ apps/kraken/src/kraken_sup.erl
@@ -0,0 +1,31 @@
+
+-module(kraken_sup).
+
+-behaviour(supervisor).
+
+%% API
+-export([start_link/0]).
+
+%% Supervisor callbacks
+-export([init/1]).
+
+%% Helper macro for declaring children of supervisor
+-define(CHILD(I, Type, Args), {I, {I, start_link, Args}, permanent, 5000, Type, [I]}).
+
+%% ===================================================================
+%% API functions
+%% ===================================================================
+
+start_link() ->
+    supervisor:start_link({local, ?MODULE}, ?MODULE, []).
+
+%% ===================================================================
+%% Supervisor callbacks
+%% ===================================================================
+
+init([]) ->
+	{ok, { {one_for_one, 5, 10}, [
+		?CHILD(kraken_server, worker, []),
+		?CHILD(kraken_listener_sup, supervisor, []),
+		?CHILD(kraken_worker_sup, supervisor, [])
+	]} }.
---
+++ apps/kraken_socket/src/kraken_socket_app.erl
@@ -0,0 +1,16 @@
+-module(kraken_socket_app).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+
+%% ===================================================================
+%% Application callbacks
+%% ===================================================================
+
+start(_StartType, _StartArgs) ->
+    kraken_socket_sup:start_link().
+
+stop(_State) ->
+    ok.
