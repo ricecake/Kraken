a6f71ed8027b1d879998829be666f068ddb17a99
Sebastian Green-Husted <ricecake@tfm.nu>
2014-01-15 21:03:54 -0500

    fill in files with templates

---
+++ src/job_workgroup_sup.erl
@@ -0,0 +1,28 @@
+
+-module({{MODULE}}).
+
+-behaviour(supervisor).
+
+%% API
+-export([start_link/0]).
+
+%% Supervisor callbacks
+-export([init/1]).
+
+%% Helper macro for declaring children of supervisor
+-define(CHILD(I, Type, Args), {I, {I, start_link, Args}, permanent, 5000, Type, [I]}).
+
+%% ===================================================================
+%% API functions
+%% ===================================================================
+
+start_link() ->
+    supervisor:start_link({local, ?MODULE}, ?MODULE, []).
+
+%% ===================================================================
+%% Supervisor callbacks
+%% ===================================================================
+
+init([]) ->
+    {ok, { {one_for_one, 5, 10}, []} }.
+
